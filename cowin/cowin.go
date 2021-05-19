1c183203971901eda287002fd26fc40c33fcd29a
MadhavanKR <madhavan.kalkunte@gmail.com>
2021-05-20 05:02:30 +0530

    added procfile

--- cowin/cowin.go
+++ cowin/cowin.go
@@ -39,7 +39,7 @@ func GetCalendarForDistrict(districtCode string) (*CalendarByDistrictResponse, e
 	calendarByDistrictRequest, _ := http.NewRequest("GET", calendarByDistrictUrl, nil)
 	//calendarByDistrictRequest.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93
     //Safari/537.36")
-    calendarByDistrictRequest.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36)"
+    calendarByDistrictRequest.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36")
 	httpClient := http.Client{}
 	response, httpErr := httpClient.Do(calendarByDistrictRequest)
 	defer response.Body.Close()
