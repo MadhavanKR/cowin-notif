c519b3f62271225ceb7ea2c9eed1196301eb6082
MadhavanKR <madhavan.kalkunte@gmail.com>
2021-05-22 00:30:33 +0530

    added subscription through commands

--- cowin/telegram_bot.go
+++ cowin/telegram_bot.go
@@ -3,32 +3,95 @@ package cowin
 import (
 	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
 	"os"
 	"time"
 )
 
 var telegramBot *tgbotapi.BotAPI
 var botCreateErr error
+var chatIds []Subscription
+
+type Subscription struct {
+	name   string `json:"name"`
+	chatId int64  `json:"chatId"`
+}
 
 func init() {
 	telegramBot, botCreateErr = tgbotapi.NewBotAPI("1897017778:AAECncT3H8vZ00aHCId6tVqetWK9P-sC8XY")
 	if botCreateErr != nil {
-		fmt.Println("error while creating telegram bot: ", botCreateErr)
+		log.Printf("error while creating telegram bot: %v\n", botCreateErr)
 		os.Exit(1)
 	}
+	chatIds = make([]Subscription, 0)
+}
+
+func clearBackLogMesages(updates tgbotapi.UpdatesChannel) {
+	updates.Clear()
 }
 
 func MessageListener() {
-
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
+
+	for {
+		updates, fetchUpdateErr := telegramBot.GetUpdatesChan(u)
+		if fetchUpdateErr != nil {
+			log.Printf("error while fetching updates from telegram bot: %v\n", fetchUpdateErr)
+			continue
+		}
+		clearBackLogMesages(updates)
+		for update := range updates {
+			if update.Message == nil {
+				continue
+			}
+
+			if update.Message.IsCommand() {
+				var replyContent string
+				switch update.Message.Command() {
+				case "help":
+					replyContent = "type /subscribe to subscribe for vaccine notifications\n " +
+						"/unsubscribe to stop receiving notifications\n" +
+						"/list to see list of subscriptions"
+					break
+				case "subscribe":
+					log.Printf("subscribing [%s] for vaccine updates", update.Message.Chat.FirstName)
+					addChatId(update.Message.Chat.ID, update.Message.Chat.FirstName)
+					replyContent = "Thanks for subscribing, you will start receiving notifications shortly"
+					break
+				case "unsubscribe":
+					removeChatId(update.Message.Chat.ID)
+					log.Printf("unsubscribing [%s] for vaccine updates", update.Message.Chat.FirstName)
+					replyContent = "You've successfully unsubscribed from vaccine notifications"
+					break
+				case "list":
+					usernames := getSubscribedUsernames()
+					fmt.Println(usernames)
+					replyContent = fmt.Sprintf("There are total of %d subscriptions, they are\n %v \n", len(usernames), usernames)
+					break
+				default:
+					log.Printf("unknown command: %s\n", update.Message.Command())
+					replyContent = "Unknown command. Please use /help to know about available commands"
+					break
+				}
+				sendMessage([]Subscription{{
+					name:   update.Message.Chat.UserName,
+					chatId: update.Message.Chat.ID,
+				}}, replyContent)
+			}
+		}
+	}
+
+}
+
+func SendVaccineUpdates(vaccineCheckInterval int, vaccine string) {
 	for {
 		calendarByDistrict, fetchErr := GetCalendarForDistrict("294")
 		if fetchErr != nil {
-			sendMessage(getChatIds(), "No vaccination centers are available")
+			sendMessage(getChatIds(), "Failed to query cowin apis")
 			continue
 		}
-		covaxCenters := GetCovaxinCenters("covaxin", calendarByDistrict)
+		covaxCenters := GetCovaxinCenters(vaccine, calendarByDistrict)
 		availableCovaxCenters := GetAvailableCenters(covaxCenters)
 		var replyContent string
 		if len(availableCovaxCenters) == 0 {
@@ -44,17 +107,46 @@ func MessageListener() {
 			}
 		}
 		sendMessage(getChatIds(), replyContent)
-		time.Sleep(15 * 60 * time.Second)
+		log.Printf("sent updates to %d chats\n", len(getChatIds()))
+		time.Sleep(time.Duration(vaccineCheckInterval) * 60 * time.Second)
 	}
 }
 
-func sendMessage(chatIds []int64, messageContent string) {
-	for _, chatId := range chatIds {
-		message := tgbotapi.NewMessage(chatId, messageContent)
+func sendMessage(chatIds []Subscription, messageContent string) {
+	for _, curSubscription := range chatIds {
+		message := tgbotapi.NewMessage(curSubscription.chatId, messageContent)
 		telegramBot.Send(message)
 	}
 }
 
-func getChatIds() []int64 {
-	return []int64{57380733, 54587111}
+func getSubscribedUsernames() []string {
+	usernames := make([]string, 0)
+	for _, curSubscription := range chatIds {
+		usernames = append(usernames, curSubscription.name)
+	}
+	return usernames
+}
+
+func removeChatId(chatId int64) {
+	index := -1
+	for i, curSubscription := range chatIds {
+		if chatId == curSubscription.chatId {
+			index = i
+			break
+		}
+	}
+	if index != -1 {
+		chatIds = append(chatIds[:index], chatIds[index+1:]...)
+	}
+}
+
+func addChatId(chatId int64, username string) {
+	chatIds = append(chatIds, Subscription{
+		name:   username,
+		chatId: chatId,
+	})
+}
+
+func getChatIds() []Subscription {
+	return chatIds
 }
--- cowinMain.go
+++ cowinMain.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"flag"
 	"github.com/MadhavanKR/cowin-notif/cowin"
 )
 
 func main() {
+	var vaccineCheckInterval int
+	flag.IntVar(&vaccineCheckInterval,"interval", 1, "--interval=3")
+	flag.Parse()
+	go cowin.SendVaccineUpdates(vaccineCheckInterval, "covaxin")
 	cowin.MessageListener()
 }
